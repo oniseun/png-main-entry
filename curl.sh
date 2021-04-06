52a10797068d6f2df1ebfb7efd54a1bc8bf6ec3e
Victor <voni@getcarbon.co>
2021-04-06 16:25:06 +0100

    channge webhook host to subnet

--- curl.sh
+++ curl.sh
@@ -1,3 +1,3 @@
-curl -X POST -H "Content-Type: application/json" -d '{ "url": "http://localhost:9000/test1"}' http://localhost:8000/subscribe/topic1 
-curl -X POST -H "Content-Type: application/json" -d '{ "url": "http://localhost:9000/test2"}' http://localhost:8000/subscribe/topic2 
+curl -X POST -H "Content-Type: application/json" -d '{ "url": "http://172.25.0.90:9000/test1"}' http://localhost:8000/subscribe/topic1 
+curl -X POST -H "Content-Type: application/json" -d '{ "url": "http://172.25.0.90:9000/test2"}' http://localhost:8000/subscribe/topic2 
 curl -X POST -H "Content-Type: application/json" -d '{"data": "hello"}' http://localhost:8000/publish/topic1
