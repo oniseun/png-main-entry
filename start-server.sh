7989b5faed34198ea9150ac5f3102233480ea41e
Victor <voni@getcarbon.co>
2021-04-06 16:23:14 +0100

    add subnets for docker-compose

--- start-server.sh
+++ start-server.sh
@@ -2,7 +2,7 @@ function run() {
    [ ! -d "pub-server" ] && git clone 'https://github.com/oniseun/pub-server.git'   
     [ ! -d "sub-server" ] && git clone 'https://github.com/oniseun/sub-server.git'
 
-    docker-compose up
+    docker-compose up --build
 }
 
 if [ -z $@ ]; then
